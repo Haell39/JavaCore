99f4283d70195e637499e530d02e8da103f5d51b
Rael <santosrafaelpro@gmail.com>
2025-05-23 10:29:14 -0300

    newprogram

---
+++ Faculdade/TestClass/Media.java
@@ -0,0 +1,32 @@
+package Faculdade.TestClass;
+import java.util.Scanner;
+
+/* Programa de calcular media  */
+/* menor 4 reprova */
+/* maior que 7 aprovado */
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a priemira nota do dicente: ");
+        float nota = sc.nextFloat();
+        System.out.println("Digite a segunda nota do dicente: ");
+        float nota2 = sc.nextFloat();
+        System.out.println("Digite a terceira nota do dicente: ");
+        float nota3 = sc.nextFloat();
+        float media = (nota + nota2 + nota3) / 3;
+
+        System.out.printf("A média do aluno é: %.2f%n", media);
+
+        if (media >= 7) {
+            System.out.println("Aluno aprovado");
+        } else if (media < 4) {
+            System.out.println("Aluno reprovado");
+        } else {
+            System.out.println("Aluno em recuperação");
+        }
+
+    }
+
+}
