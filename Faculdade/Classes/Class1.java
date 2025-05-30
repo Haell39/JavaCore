cd4699f857147105e33b44a19d86616020035b07
Rael <santosrafaelpro@gmail.com>
2025-05-30 09:45:34 -0300

    Pessoa

--- Faculdade/Classes/Class1.java
+++
@@ -1,5 +0,0 @@
-package Faculdade.Classes;
-
-public class Class1 {
-    
-}
---
+++ Faculdade/Pessoa/Pessoa.java
@@ -0,0 +1,15 @@
+package Faculdade.Pessoa;
+
+public class Pessoa {
+    // Classe Pessoa:
+    private String nome;
+    private String cpf;
+    private int idade;
+    private String email;
+    private String telefone;
+    private int CEP;
+}
+
+class Aluno extends  Pessoa {
+
+}
